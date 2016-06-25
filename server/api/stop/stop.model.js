1f63ae8f07fc72d61ccc7d2c5fc35b2381912375
Alessandro Annini <alessandro.annini@gmail.com>
2016-06-25 10:33:57 +0200

    new user seed and check endpoint

--- server/api/stop/stop.model.js
+++ server/api/stop/stop.model.js
@@ -3,12 +3,6 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-var StopSchema = new Schema({
-  // name: String,
-  // info: String,
-  // active: Boolean
-}, {
-  strict: false
-});
+var StopSchema = new Schema({}, { strict: false });
 
 module.exports = mongoose.model('Stop', StopSchema);
--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,7 @@ var path = require('path');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/checks', require('./api/check'));
   app.use('/api/stops', require('./api/stop'));
   app.use('/api/lines', require('./api/line'));
   app.use('/api/things', require('./api/thing'));
